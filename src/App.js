4114bff65dae403a31d1c370ac2f9c91cd67b148
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-11-08 08:11:55 +0100

    Animate sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import { useState } from 'react';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <div className="App">
-      <Sidebar />
+      <button onClick={openSidebar}>Open sidebar</button>
+      <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-const Sidebar = () => {
-    return (
-        <div className='sidebar'>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Services</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-    )
-}
+const Sidebar = ({ isOpen, onClose }) => {
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="sidebar"
+          initial={{ x: -300 }}
+          animate={{ x: 0 }}
+          exit={{ x: -300 }}
+        >
+          <button className="close-button" onClick={onclose}>
+            Close
+          </button>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
 
 export default Sidebar;
