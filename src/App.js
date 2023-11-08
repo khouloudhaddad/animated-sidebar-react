6bcbd3391f0a327ebc5073f1905e18646f9a721d
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-11-08 09:04:01 +0100

    Finish stylying sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Sidebar from './components/Sidebar';
 import { useState } from 'react';
 
@@ -15,7 +13,23 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={openSidebar}>Open sidebar</button>
+      <button onClick={openSidebar} className="open_sidebar">
+        <span className="hidden">Open sidebar</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="#000"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          ></path>
+        </svg>
+      </button>
       <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,61 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiPhoneCall } from "react-icons/bi";
+import { BsBoxes } from "react-icons/bs";
+import { BsInfoCircle } from "react-icons/bs";
 
 const Sidebar = ({ isOpen, onClose }) => {
   return (
     <AnimatePresence>
       {isOpen && (
         <motion.div
-          className="sidebar"
+          className="sidebar p-8"
           initial={{ x: -300 }}
           animate={{ x: 0 }}
           exit={{ x: -300 }}
         >
-          <button className="close-button" onClick={onclose}>
-            Close
+          <button className="close_button" onClick={onClose}>
+            <span className="hidden">Close sidebar</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
           </button>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Contact</li>
+          <div className="logo_wrapper">
+            <img
+              className="logo"
+              src="https://tailwindui.com/img/logos/mark.svg?color=white"
+              alt="Your Company"
+            />
+          </div>
+          <ul className="navigation">
+            <li className="py-3 flex items-center active mb-2">
+              <AiOutlineHome />
+              <span className="ml-2">Home</span>
+            </li>
+            <li className="py-3 flex items-center mb-2">
+              <BsInfoCircle />
+              <span className="ml-2">About</span>
+            </li>
+            <li className="py-3 flex items-center mb-2">
+              <BsBoxes />
+              <span className="ml-2">Services</span>
+            </li>
+            <li className="py-3 flex items-center mb-2">
+              <BiPhoneCall />
+              <span className="ml-2">Contact</span>
+            </li>
           </ul>
         </motion.div>
       )}
